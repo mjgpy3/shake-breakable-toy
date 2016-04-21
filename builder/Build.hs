fe12ce6f6c41b264563f9c095679651cce45130c
Michael Gilliland <mjg.py3@gmail.com>
2016-04-21 19:42:16 -0400

    Remove demo cruft

--- builder/Build.hs
+++ builder/Build.hs
@@ -8,23 +8,7 @@ main = do
   shakeArgs shakeOptions{shakeFiles="_build"} $ do
     want ["build-node-project"]
 
-    phony "clean" $ do
-      putNormal "Cleaning files in _build"
-      removeFilesAfter "_build" ["//*"]
-
     phony "build-node-project" $ do
       putNormal "Building a node project"
       need ["../node-project" </> "Dockerfile"]
       cmd "docker build -t" ["foo/image"] "../node-project/"
-
-    "_build/run" <.> exe %> \out -> do
-        cs <- getDirectoryFiles "" ["//*.c"]
-        let os = ["_build" </> c -<.> "o" | c <- cs]
-        need os
-        cmd "gcc -o" [out] os
-
-    "_build//*.o" %> \out -> do
-        let c = dropDirectory1 $ out -<.> "c"
-        let m = out -<.> "m"
-        () <- cmd "gcc -c" [c] "-o" [out] "-MMD -MF" [m]
-        needMakefileDependencies m
